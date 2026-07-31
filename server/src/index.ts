0a914b5a9cdad653b005bace7494e01bff4450ce
Shambbhavi-coder <shambhavi3jul@gmail.com>
2026-07-31 16:45:33 +0530

    made changes to index.tsx

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,3 @@
-import express, { Express } from "express";
-import mongoose from "mongoose";
-import financialRecordRouter from "./routes/financial-records";
-import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-mongoose.connect(process.env.MONGO_URI!);
-const app: Express = express();
-const port = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(cors());
-
 
 
 
